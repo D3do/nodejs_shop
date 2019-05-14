69e2d5e047c2486429a5c7c08e0bd3e579385bd9
Sebastian Kozakiewicz <skkozakiewicz@gmail.com>
2019-05-14 20:37:55 +0200

    change form styling

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,7 +11,10 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const {title, imageUrl, price, description} = req.body;
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
@@ -25,4 +28,4 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products'
     });
   });
-}
+};
--- controllers/shop.js
+++ controllers/shop.js
@@ -4,8 +4,8 @@ exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
       prods: products,
-      pageTitle: 'All products',
-      path: '/products',
+      pageTitle: 'All Products',
+      path: '/products'
     });
   });
 };
@@ -18,18 +18,25 @@ exports.getIndex = (req, res, next) => {
       path: '/'
     });
   });
-}
+};
 
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
     pageTitle: 'Your Cart'
-  })
-}
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
-    path: 'checkout',
+    path: '/checkout',
     pageTitle: 'Checkout'
-  })
-}
+  });
+};
