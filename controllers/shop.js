4ba99d3ae697dc7ec3606d77949b4199702069d4
Sebastian Kozakiewicz <skkozakiewicz@gmail.com>
2019-05-14 23:21:18 +0200

    add details view

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,9 +13,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const productId = req.params.productId;
   Product.findById(productId, product => {
-    console.log(product);
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    })
   });
-  res.redirect('/');
 };
 
 exports.getIndex = (req, res, next) => {
@@ -33,6 +36,12 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId;
+  console.log(productId);
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
