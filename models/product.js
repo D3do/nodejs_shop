b1873948beda3e9a3eec616cc552377d50a04fda
Sebastian Kozakiewicz <skkozakiewicz@gmail.com>
2019-06-06 23:34:19 +0200

    add deleteById functionality

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -45,14 +47,30 @@ module.exports = class Product {
     });
   }
 
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if(!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      });
+    })
+  }
+
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
 
   static findById(id, cb) {
     getProductsFromFile(products => {
-      const product = products.find(product => product.id === id);
-      cb(product);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if(!err) {
+
+        }
+      });
     })
   }
 };
